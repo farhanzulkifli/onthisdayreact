efe79138e4ccd722fed8521c4712d2d2c4236bb5
farhan <mzulfarhan94@gmail.com>
2021-07-07 09:51:54 +0800

    trial 2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import NotFoundPage from "./Components/NotFoundPage";
-import Display from "./Components/Display";
 import DatePickers3 from "./Components/Datepickers3";
 import Events2 from "./Components/Events2";
 const App = () => {
@@ -14,7 +13,6 @@ const App = () => {
         <Route exact path = "/" component = {DatePickers3}/>
         <Route exact path = "/404" component = {NotFoundPage}/>
         <Route exact path = "/:month/:day/events" component ={Events2}/>
-        <Route exact path = "/display" component ={Display}/>
         <Redirect to = "/404"/>
           </Switch>
     </Router>
